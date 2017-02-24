9528b0c214c7e7ffe436e8d75611252988a2c7a7
denimskater <drew.vanzandt@gmail.com>
2017-02-24 03:57:27 +0000

    Started adding code for sending commands to scope

--- scopebuddy.py
+++ scopebuddy.py
@@ -5,7 +5,9 @@
 
 import speech_recognition as sr
 
-#Scope Identification
+#Scope Identification requires an execution of a VX11 command in this program
+#FIX ME ADD VX11 COMMAND
+
 scopeIdRead = 'Tektronix'
 scopeMfg = ''
 if 'LeCroy' in scopeIdRead:
@@ -18,7 +20,7 @@ print(scopeMfg)
 # obtain audio from the microphone
 r = sr.Recognizer()
 with sr.Microphone(device_index = 2, sample_rate=48000, chunk_size = 512) as source:
-    r.adjust_for_ambient_noise(source) # listen for 1 second to calibrate the energy threshold for ambient noise levels
+    #r.adjust_for_ambient_noise(source) # listen for 1 second to calibrate the energy threshold for ambient noise levels
     print("Say something!")
     audio = r.listen(source)
 
@@ -31,4 +33,25 @@ except sr.UnknownValueError:
     print("Sphinx could not understand audio")
 except sr.RequestError as e:
     print("Sphinx error; {0}".format(e))
+
+# send audio packet back to scope also using VX11 commands
+
+audioPacket = r.recognize_sphinx(audio)
+print(type(audioPacket))
+
+#if 'scope' in audio:
+#    if 'stop' in audio:
+#        audioPacket = 'stop'
+#    elif 'run' in audio:
+#        audioPacket = 'run'
+#    elif 'single' in audio:
+#        audioPacket = 'single'
+#    elif 'auto' in audio:
+#        audioPacket = 'auto'
+#print(audio)
+print(audioPacket)
+   
+
+
+
 
